88e3c12ad44a053a542ec5900c6967bfbaaab97e
kwangcheollee <leekwangchul@hanmail.net>
2016-07-22 11:23:26 +0900

    first commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String myId, myPWord;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    Button btn = (Button)findViewById(R.id.btn);
+    btn.s
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(this,"toast test",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
